abfdc1cc3b7fffc8281e6e090837a83b89a2dc6a
Trystan Gray <trystangray7@gmail.com>
2019-04-11 09:08:33 -0600

    uploading first part of snap challenge

---
+++ snap-04-11/snap-04-11-erd.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Entity</title>
+	</head>
+	<body>
+		<h1>ERD</h1>
+		<img src="ERD-snap-04-11.jpg" alt="no pic:(">
+		<h4><a href="snap-04-11.php">User Story</a></h4>
+	</body>
+</html>
---
+++ snap-04-11/snap-04-11.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>User Story</title>
+	</head>
+	<body>
+		<h1>User Story</h1>
+		<p>As a moderate/ heavy user I can upload a picture on Mail Chimp.</p>
+		<h4><a href="snap-04-11-erd.php">ERD</a></h4>
+	</body>
+</html>
